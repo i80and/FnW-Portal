710fce7e08d126a54e7b5c8da16c26610d61d513
Sasha Elaine Fox <sashanullptr@gmail.com>
2019-11-11 16:21:11 -0700

    Support css loading.

--- webpack.config.js
+++ webpack.config.js
@@ -16,11 +16,15 @@ module.exports = {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: ['babel-loader']
+            },
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
             }
         ]
     },
     resolve: {
-        extensions: ['*', '.js', '.jsx']
+        extensions: ['*', '.js', '.jsx', '.css']
     },
     output: {
         path: path.join(__dirname, 'dist'),
