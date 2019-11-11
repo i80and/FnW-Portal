74e09f89892e6035bdcc36dd7393f5a9463795ec
Sasha Elaine Fox <sashanullptr@gmail.com>
2019-11-11 14:45:47 -0700

    Minimum viable set-up for React, Express, Webpack and Babel.

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: "./src/index.html",
+    filename: "./index.html"
+});
+
+module.exports = {
+    entry: path.join(__dirname, "src", "js", "app.js"),
+    plugins: [htmlPlugin],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: "[name].js",
+        publicPath: '/',
+    },
+    devServer: {
+        contentBase: './dist'
+    }
+};
