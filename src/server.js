9b4cb3b1c38d255cbaba722c58ca86059c0fcdd8
Sasha Elaine Fox <sashanullptr@gmail.com>
2019-11-11 18:05:36 -0700

    Remove redundant requires

--- src/server.js
+++ src/server.js
@@ -7,7 +7,6 @@ const port = process.env.PORT || 3000;
 const DIST_DIR = path.join(__dirname, '../dist');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
 
-const cors = require('cors');
 app.set('trust proxy', '127.0.0.1');
 
 app.use(express.static(DIST_DIR));
