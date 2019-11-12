85e209beb03a1c1bb874c872309a9b98a116a0c4
Sasha Elaine Fox <sashanullptr@gmail.com>
2019-11-11 18:08:16 -0700

    Trust all proxies.

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 const DIST_DIR = path.join(__dirname, '../dist');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
 
-app.set('trust proxy', '127.0.0.1');
+app.set('trust proxy');
 
 app.use(express.static(DIST_DIR));
 
