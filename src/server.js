68639765c833a84d159f12e84b7d1d907b5b844f
Sasha Elaine Fox <sashanullptr@gmail.com>
2019-11-11 18:04:37 -0700

    Add CORS support.

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const path = require('path'); // NEW
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 const DIST_DIR = path.join(__dirname, '../dist');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
 
+const cors = require('cors');
 app.use(express.static(DIST_DIR));
 
 app.get('/', (req, res) => {
